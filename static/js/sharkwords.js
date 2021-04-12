e41d642af804896128500c8b6d0d89c1dbe14eb3
Abby Chau <abzchau@gmail.com>
2021-04-12 04:27:28 +0000

    Adds Functionality For Disabled Buttons

---
+++ static/js/sharkwords.js
@@ -0,0 +1,100 @@
+const ALPHABET = 'abcdefghijklmnopqrstuvwxyz';
+const WORDS = [
+  'strawberry',
+  'orange',
+  'apple',
+  'banana',
+  'pineapple',
+  'kiwi',
+  'peach',
+  'pecan',
+  'eggplant',
+  'durian',
+  'peanut',
+  'chocolate',
+];
+
+let numWrong = 0;
+
+// Loop over the chars in `word` and create divs.
+//
+const createDivsForChars = (word) => {
+  for (const i in word) {
+    $('#word-container').append(`<div class="letter-box ${word[i]}"></div>`);
+  }
+}; 
+
+// Loop over each letter in `ALPHABET` and generate buttons.
+//
+const generateLetterButtons = () => {
+  for (const i in ALPHABET) {
+    $('#letter-buttons').append(`<button>${ALPHABET[i]}</button>`)
+  }
+};
+
+// Set the `disabled` property of `buttonEl` to `true.
+//
+// `buttonEl` is an `HTMLElement` object.
+//
+const disableLetterButton = (buttonEl) => {
+  
+  // buttonEl.get(0).disabled = true
+  buttonEl.prop("disabled", true)
+};
+
+// Return `true` if `letter` is in the word.
+//
+const isLetterInWord = (letter) => {
+  // Replace this with your code
+};
+
+// Called when `letter` is in word. Update contents of divs with `letter`.
+//
+const handleCorrectGuess = (letter) => {
+  // Replace this with your code
+};
+
+// Called when `letter` is not in word.
+//
+// If the shark gets the person, disable all buttons and show the "play again"
+// message. Otherwise, increment `numWrong` and update the shark image.
+//
+const handleWrongGuess = () => {
+  // Replace this with your code
+};
+
+//  Reset game state. Called before restarting the game.
+//
+const resetGame = () => {
+  window.location = '/sharkwords';
+};
+
+// This is like if __name__ == '__main__' in Python
+//
+(function startGame() {
+  // For now, we'll hardcode the word that the user has to guess.
+  const word = 'hello';
+
+  createDivsForChars(word);
+  generateLetterButtons();
+
+  $('button').on('click', (evt) => {
+
+    const clickedBtn = $(evt.target);
+    // console.log(evt.target)
+    // console.log(clickedBtn)
+    disableLetterButton(clickedBtn);
+
+    const letter = clickedBtn.html();
+
+    if (isLetterInWord(letter)) {
+      handleCorrectGuess(letter);
+    } else {
+      handleWrongGuess(letter);
+    }
+  });
+
+  $('#play-again').on('click', () => {
+    resetGame();
+  });
+})();
